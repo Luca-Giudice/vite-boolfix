9c7ecf66d7e5f3e32de577a8e9fd1987b6ad39c2
Luca-Giudice <2giudice4@gmail.com>
2023-01-30 15:14:41 +0100

    milestone_1

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,31 @@ export default {
   name: 'Boolflix',
   components: {SearchBar},
   data: () => ({store, titleFilter: ''}),
+  computed: {
+    axiosConfig(){
+      const {key, language} = api;
+      return{
+        params: {
+          language: language,
+          api_key: key,
+          query: this.titlefilter
+        }
+      }
+    }
+  };
   methods: {
     umdateTitleFilter(term){
       this.titleFilter = term
     },
     searchMovies(){
-      //
-      axios.get()
+     if(!titleFilter) {
+      this.movies =[];
+      return;
+     }
+      const { key, baseUri, language} = api;
+      axios.get(`${baseUri}/search/movie`, this.axiosConfig).then(res => {
+        store.movies = res.data.results
+      }).catch(err => {console.error(err)});
     }
   }
 };
@@ -23,6 +41,27 @@ export default {
   <header>
     <search-bar placeholder="Cerca un film" @term-change="updateTitleFilter"></search-bar>
   </header>
+  <main>
+    <section>
+      <h2>
+        Movies
+      </h2>
+      <ul v-for="movie in store.movies" :key="movie.id">
+        <li>
+          {{movie.title}}
+        </li>
+        <li>
+          {{ movie.original_title }}
+        </li>
+        <li>
+          {{ movie.original_language }}
+        </li>
+        <li>
+          {{ movie.vote_avarage }}
+        </li>
+      </ul>
+    </section>
+  </main>
 
 
 </template>
